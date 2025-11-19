9766c52b69328ab9580f21984693b3f3ee5175a9
alexsantos-eth <alexdsantosv@gmail.com>
2025-11-19 12:58:28 -0600

    [ fix ] footer

--- src/layout/components/footer.tsx
+++ src/layout/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="h-[20dvh] bg-black w-full relative p-8 z-10">
+    <div className="h-[30dvh] bg-black w-full relative p-8 z-10">
       <div className="flex flex-row items-center gap-8">
         <img src="/images/logo.png" alt="Zafnat Logo" className="h-8 w-auto" />
         <div>
@@ -8,37 +8,26 @@ const Footer = () => {
             © 2025 Zafnat Group — Innovación agrícola para un futuro sostenible.
           </p>
           <div>
-            <a className="text-white underline text-xs" href="/privacy">
-              Política de Privacidad
-            </a>{" "}
-            {" | "}
-            <a className="text-white underline text-xs" href="/terms">
-              Términos
-            </a>{" "}
-            {" | "}
             <a
               className="text-white underline text-xs"
-              href="https://www.linkedin.com/company/zafnat"
+              href="#"
               target="_blank"
               rel="noopener noreferrer"
             >
-              LinkedIn
+              Tiktk
             </a>{" "}
             {" | "}
             <a
               className="text-white underline text-xs"
-              href="https://www.youtube.com/@zafnat"
+              href="#"
               target="_blank"
               rel="noopener noreferrer"
             >
               YouTube
             </a>{" "}
             {" | "}
-            <a
-              className="text-white underline text-xs"
-              href="mailto:contacto@zafnat.com"
-            >
-              Correo de contacto
+            <a className="text-white underline text-xs" href="#">
+              Instagram
             </a>
           </div>
         </div>
